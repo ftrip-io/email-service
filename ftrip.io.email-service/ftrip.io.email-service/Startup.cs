a5f5cf744c0e940f66b75eca5360d1e72e319287
Panic <panic.milos99@gmail.com>
2023-05-05 14:39:51 +0200

    Setup/initial project (#3)
    * feat: initial email service with contracts library
    
    * feat: framework setup, dockerfiles

---
+++ ftrip.io.email-service/ftrip.io.email-service/Startup.cs
@@ -0,0 +1,64 @@
+using ftrip.io.framework.ExceptionHandling.Extensions;
+using ftrip.io.framework.HealthCheck;
+using ftrip.io.framework.Installers;
+using ftrip.io.framework.Mapping;
+using ftrip.io.framework.messaging.Installers;
+using ftrip.io.framework.Persistence.NoSql.Mongodb.Installers;
+using ftrip.io.framework.Validation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ftrip.io.email_service
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            InstallerCollection.With(
+                new HealthCheckUIInstaller(services),
+                new AutoMapperInstaller<Startup>(services),
+                new FluentValidationInstaller<Startup>(services),
+                new MongodbInstaller(services),
+                new MongodbHealthCheckInstaller(services),
+                new RabbitMQInstaller<Startup>(services, RabbitMQInstallerType.Consumer)
+            ).Install();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseFtripioGlobalExceptionHandler();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseFtripioHealthCheckUI(Configuration.GetSection(nameof(HealthCheckUISettings)).Get<HealthCheckUISettings>());
+        }
+    }
+}
