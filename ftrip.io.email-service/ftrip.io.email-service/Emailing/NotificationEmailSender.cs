5bc8e2e8854c380b428e7e919b3cc69892d92278
Panic <panic.milos99@gmail.com>
2023-05-11 21:14:05 +0200

    feat/email-notification-sender-#2 (#8)
    * feat: #2 email notifications
    
    * refactor: #2 email in template

---
+++ ftrip.io.email-service/ftrip.io.email-service/Emailing/NotificationEmailSender.cs
@@ -0,0 +1,45 @@
+﻿using EmailService.Constracts;
+using EmailService.Models;
+using ftrip.io.email_service.ContactsList;
+using ftrip.io.notification_service.contracts.Notifications.Events;
+using MassTransit;
+using System.Threading.Tasks;
+
+namespace ftrip.io.email_service.Emailing
+{
+    public class NotificationEmailSender : IConsumer<NotificationSavedEvent>
+    {
+        private readonly IContactRepository _contactRepository;
+        private readonly ITemplatesProvider _templatesProvider;
+        private readonly IEmailDispatcher _emailDispatcher;
+
+        public NotificationEmailSender(
+            IContactRepository contactRepository,
+            ITemplatesProvider templatesProvider,
+            IEmailDispatcher emailDispatcher)
+        {
+            _contactRepository = contactRepository;
+            _templatesProvider = templatesProvider;
+            _emailDispatcher = emailDispatcher;
+        }
+
+        public async Task Consume(ConsumeContext<NotificationSavedEvent> context)
+        {
+            var notificationSaved = context.Message;
+
+            var contact = await _contactRepository.ReadByUserId(notificationSaved.UserId);
+            if (contact == null)
+            {
+                return;
+            }
+
+            var email = _templatesProvider.FromTemplate<Email>("NewNotification", new
+            {
+                To = contact.Email,
+                contact.Name
+            });
+
+            _emailDispatcher.Dispatch(email);
+        }
+    }
+}
