3dc60a70c6eb7ee54896e68ea7100744b5d7469e
Panic <panic.milos99@gmail.com>
2023-05-16 21:33:26 +0200

    feat: logging setup and logging messages

--- ftrip.io.email-service/ftrip.io.email-service/ContactsList/Consumers/UserCreatedEventConsumer.cs
+++ ftrip.io.email-service/ftrip.io.email-service/ContactsList/Consumers/UserCreatedEventConsumer.cs
@@ -1,6 +1,7 @@
 ﻿using ftrip.io.email_service.ContactsList.Domain;
 using ftrip.io.user_service.contracts.Users.Events;
 using MassTransit;
+using Serilog;
 using System.Threading.Tasks;
 
 namespace ftrip.io.email_service.ContactsList.Consumers
@@ -8,22 +9,28 @@ namespace ftrip.io.email_service.ContactsList.Consumers
     public class UserCreatedEventConsumer : IConsumer<UserCreatedEvent>
     {
         private readonly IContactRepository _contactRepository;
+        private readonly ILogger _logger;
 
-        public UserCreatedEventConsumer(IContactRepository contactRepository)
+        public UserCreatedEventConsumer(
+            IContactRepository contactRepository,
+            ILogger logger)
         {
             _contactRepository = contactRepository;
+            _logger = logger;
         }
 
         public async Task Consume(ConsumeContext<UserCreatedEvent> context)
         {
             var userCreatedEvent = context.Message;
 
-            await _contactRepository.Create(new Contact()
+            var createdContact = await _contactRepository.Create(new Contact()
             {
                 UserId = userCreatedEvent.UserId,
                 Email = userCreatedEvent.Email,
                 Name = userCreatedEvent.FirstName + " " + userCreatedEvent.LastName
             });
+
+            _logger.Information("Created contact - UserId[{UserId}], Email[{Email}]", createdContact.UserId, createdContact.Email);
         }
     }
 }
--- ftrip.io.email-service/ftrip.io.email-service/Emailing/NotificationEmailSender.cs
+++ ftrip.io.email-service/ftrip.io.email-service/Emailing/NotificationEmailSender.cs
@@ -3,6 +3,7 @@ using EmailService.Models;
 using ftrip.io.email_service.ContactsList;
 using ftrip.io.notification_service.contracts.Notifications.Events;
 using MassTransit;
+using Serilog;
 using System.Threading.Tasks;
 
 namespace ftrip.io.email_service.Emailing
@@ -12,15 +13,18 @@ namespace ftrip.io.email_service.Emailing
         private readonly IContactRepository _contactRepository;
         private readonly ITemplatesProvider _templatesProvider;
         private readonly IEmailDispatcher _emailDispatcher;
+        private readonly ILogger _logger;
 
         public NotificationEmailSender(
             IContactRepository contactRepository,
             ITemplatesProvider templatesProvider,
-            IEmailDispatcher emailDispatcher)
+            IEmailDispatcher emailDispatcher,
+            ILogger logger)
         {
             _contactRepository = contactRepository;
             _templatesProvider = templatesProvider;
             _emailDispatcher = emailDispatcher;
+            _logger = logger;
         }
 
         public async Task Consume(ConsumeContext<NotificationSavedEvent> context)
@@ -30,6 +34,7 @@ namespace ftrip.io.email_service.Emailing
             var contact = await _contactRepository.ReadByUserId(notificationSaved.UserId);
             if (contact == null)
             {
+                _logger.Warning("Could not send email notification because contact is not found - UserId[{UserId}]", notificationSaved.UserId);
                 return;
             }
 
@@ -40,6 +45,7 @@ namespace ftrip.io.email_service.Emailing
             });
 
             _emailDispatcher.Dispatch(email);
+            _logger.Information("Email notification sent - NotificationId[{NotificationId}], UserId[{UserId}], Email[{Email}]", notificationSaved.NotificationId, contact.UserId, contact.Email);
         }
     }
 }
