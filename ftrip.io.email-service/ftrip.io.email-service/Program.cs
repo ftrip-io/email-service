0959c6bfec12f8c3794e9a6109fd73282b332306
Panic <panic.milos99@gmail.com>
2023-05-18 22:00:01 +0200

    Feat/logging (#9)
    * feat: logging setup and logging messages
    
    * feat: updated framework to 2.5.1

--- ftrip.io.email-service/ftrip.io.email-service/Program.cs
+++ ftrip.io.email-service/ftrip.io.email-service/Program.cs
@@ -1,5 +1,7 @@
+using ftrip.io.framework.Logging;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace ftrip.io.email_service
 {
@@ -12,6 +14,17 @@ namespace ftrip.io.email_service
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilogLogging((hostingContext) =>
+                {
+                    return new LoggingOptions()
+                    {
+                        ApplicationName = hostingContext.HostingEnvironment.ApplicationName,
+                        ApplicationLabel = "email",
+                        ClientIdAttribute = "X-Forwarded-For",
+                        GrafanaLokiUrl = Environment.GetEnvironmentVariable("GRAFANA_LOKI_URL"),
+                        ConsoleOutputTemplate = "[{Timestamp:yyyy-MM-dd HH:mm:ss} {Level:u3}] - {Message:lj}{NewLine}"
+                    };
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
