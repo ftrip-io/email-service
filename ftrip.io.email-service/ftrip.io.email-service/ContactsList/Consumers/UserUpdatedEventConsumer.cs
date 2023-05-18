4eacb96820fc7ccc58e5db1b6bf80127c7a40ec2
Panic <panic.milos99@gmail.com>
2023-05-18 22:00:01 +0200

    Feat/logging (#9)
    * feat: logging setup and logging messages
    
    * feat: updated framework to 2.5.1

--- ftrip.io.email-service/ftrip.io.email-service/ContactsList/Consumers/UserUpdatedEventConsumer.cs
+++ ftrip.io.email-service/ftrip.io.email-service/ContactsList/Consumers/UserUpdatedEventConsumer.cs
@@ -1,5 +1,6 @@
 ﻿using ftrip.io.user_service.contracts.Users.Events;
 using MassTransit;
+using Serilog;
 using System.Threading.Tasks;
 
 namespace ftrip.io.email_service.ContactsList.Consumers
@@ -7,10 +8,14 @@ namespace ftrip.io.email_service.ContactsList.Consumers
     public class UserUpdatedEventConsumer : IConsumer<UserUpdatedEvent>
     {
         private readonly IContactRepository _contactRepository;
+        private readonly ILogger _logger;
 
-        public UserUpdatedEventConsumer(IContactRepository contactRepository)
+        public UserUpdatedEventConsumer(
+            IContactRepository contactRepository,
+            ILogger logger)
         {
             _contactRepository = contactRepository;
+            _logger = logger;
         }
 
         public async Task Consume(ConsumeContext<UserUpdatedEvent> context)
@@ -20,13 +25,15 @@ namespace ftrip.io.email_service.ContactsList.Consumers
             var contact = await _contactRepository.ReadByUserId(userUpdatedEvent.UserId);
             if (contact == null)
             {
+                _logger.Warning("Could not update contact because it is not found - UserId[{UserId}]", userUpdatedEvent.UserId);
                 return;
             }
 
             contact.Email = userUpdatedEvent.Email;
             contact.Name = userUpdatedEvent.FirstName + " " + userUpdatedEvent.LastName;
 
-            await _contactRepository.Update(contact);
+            var updatedContact = await _contactRepository.Update(contact);
+            _logger.Information("Updated contact - UserId[{UserId}], Email[{Email}]", updatedContact.UserId, updatedContact.Email);
         }
     }
 }
