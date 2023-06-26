7d3de57aacd726b9fc1b076e01954f8a247d37ed
Panic <panic.milos99@gmail.com>
2023-06-26 21:49:44 +0200

    feat: installing current user context. (#15)

---
+++ ftrip.io.email-service/ftrip.io.email-service/Installers/CurrentUserInstaller.cs
@@ -0,0 +1,21 @@
+﻿using ftrip.io.framework.Contexts;
+using ftrip.io.framework.Installers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ftrip.io.email_service.Installers
+{
+    public class CurrentUserInstaller : IInstaller
+    {
+        private readonly IServiceCollection _services;
+
+        public CurrentUserInstaller(IServiceCollection services)
+        {
+            _services = services;
+        }
+
+        public void Install()
+        {
+            _services.AddScoped(typeof(CurrentUserContext));
+        }
+    }
+}
